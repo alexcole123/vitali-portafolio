6708db48a849c9901aaea4185be7078aff926e95
alexj.cole@hotmail.com <alexj.cole@hotmail.com>
2025-08-12 22:55:39 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vitali-portafolio/',  // <-- add this line with your repo name
   plugins: [react()],
+  base: '/vitali-portafolio/', // Add this line!
 })
