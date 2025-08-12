9689263cee73ee057ba2238e95437feb7fbc1ee9
alexj.cole@hotmail.com <alexj.cole@hotmail.com>
2025-08-12 21:27:00 +0300

    improvements for mobile

--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import PullToRefresh from 'react-pull-to-refresh';
 import type { JSX } from "react";
 import { Copyrights } from "../Copyrights/Copyrights";
 import { Header } from "../Header/Header";
@@ -6,25 +7,41 @@ import { Routing } from "../Routing/Routing";
 import "./Layout.css";
 
 export function Layout(): JSX.Element {
+    
+    const handleRefresh = async () => {
+        // You can refresh different things based on current page
+        console.log('Refreshing page...');
+        
+        // Simple page reload (easiest approach)
+        window.location.reload();
+        
+        // OR more sophisticated: check current route and refresh accordingly
+        // const currentPath = window.location.pathname;
+        // if (currentPath === '/portafolio') {
+        //     await refreshPortfolio();
+        // } else if (currentPath === '/home') {
+        //     await refreshHome();
+        // }
+    };
+
     return (
-        <div className="Layout">
-			<header>
-                <Header />
-            <aside>
-                < Menu />
-            </aside> 
-            </header>
+        <PullToRefresh onRefresh={handleRefresh}>
+            <div className="Layout">
+                <header>
+                    <Header />
+                    <aside>
+                        <Menu />
+                    </aside> 
+                </header>
 
-
-            <main>
-                <Routing />
-            </main>
-            
-            <footer>
-                <Copyrights />
-            </footer>
-
-
-        </div>
+                <main>
+                    <Routing />
+                </main>
+                
+                <footer>
+                    <Copyrights />
+                </footer>
+            </div>
+        </PullToRefresh>
     );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/vitali-portafolio/',  // <-- add this line with your repo name
   plugins: [react()],
 })
